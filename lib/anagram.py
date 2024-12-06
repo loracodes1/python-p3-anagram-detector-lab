dc77711af0472f310201a78a14ce522f702857be
florence <loracodes3@gmail.com>
2024-12-06 23:13:17 +0300

    anagram detection

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,9 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, possible_anagrams):
+        # Sort the original word and compare it with each word in the list
+        return [word for word in possible_anagrams if sorted(word) == sorted(self.word)]
